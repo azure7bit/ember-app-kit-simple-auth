5a70f49c7c13848ce0fd9239051d1bf61aba8e5c
erkarl <karl@karlranna.com>
2013-11-02 20:41:54 +0200

    Added ember-simple-auth to bower dependencies and initalized Ember.SimpleAuth.

--- app/app.js
+++ app/app.js
@@ -15,6 +15,11 @@ App.initializer({
   name: 'Register Components',
   initialize: function(container, application) {
     registerComponents(container);
+    // Options for Auth
+    var options = {
+        serverTokenEndpoint: "http://localhost:7000/oauth2/access_token/"
+    };
+    Ember.SimpleAuth.setup(container, application, options);
   }
 });
 
