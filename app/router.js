15201017be70b766682a24b5783ec8a195ab2ccd
erkarl <karl@karlranna.com>
2013-11-02 21:05:07 +0200

    Added example protected route+template and controller for auth.

--- app/router.js
+++ app/router.js
@@ -1,11 +1,10 @@
 var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
 
 Router.map(function() {
-  this.route('component-test');
-  this.route('helper-test');
-  // this.resource('posts', function() {
-  //   this.route('new');
-  // });
+  // Auth-example
+  this.route('index', { path: '/' });
+  this.route('protected');
+  this.route('login');
 });
 
 export default Router;
